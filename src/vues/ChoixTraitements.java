c34645125d1f71239b8bcf41ad34c03edb155894
Jsbeaulne <jeansebastien321@gmail.com>
2019-04-23 22:36:54 -0400

    NeVeR

--- src/vues/ChoixTraitements.java
+++ src/vues/ChoixTraitements.java
@@ -4,6 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -24,6 +27,52 @@ public class ChoixTraitements extends JFrame {
 		setIconImage(new ImageIcon("icon.png").getImage());
 
 		JPanel p = new JPanel(new GridBagLayout());
+		
+		String url = "https://bit.ly/IqT6zt";
+		
+		JButton b1 = new JButton("Artiste");
+		b1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				new AffichageGestionArtiste();
+				dispose();
+			}
+			
+		});
+		JButton b2 = new JButton("Album");
+		b2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				new AffichageGestionAlbum();
+				dispose();
+			}
+			
+		});
+		JButton b3 = new JButton("Aide en ligne");
+		b3.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		});
+		JButton b4 = new JButton("Quitter");
+		b4.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+			
+		});
 
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.anchor = GridBagConstraints.CENTER;
@@ -39,10 +88,10 @@ public class ChoixTraitements extends JFrame {
 		p.add(new JLabel("Sélectionner la catégorie à modifier"), gbc);
 
 		gbc.gridy = 2;
-		p.add(new JButton("Artiste"), gbc); //add action listnener
+		p.add(b1, gbc);
 
 		gbc.gridy = 3;
-		p.add(new JButton("Album"), gbc); //add action listnener
+		p.add(b2, gbc);
 
 		add(p, BorderLayout.NORTH);
 
@@ -51,28 +100,14 @@ public class ChoixTraitements extends JFrame {
 		gbc1.anchor = GridBagConstraints.WEST;
 		gbc1.fill = GridBagConstraints.NONE;
 		gbc1.insets = new Insets(10, 10, 10, 10);
-		p1.add(new JButton("Aide en ligne"), gbc1); //add action listnener
+		p1.add(b3, gbc1);
 
 		gbc1.anchor = GridBagConstraints.EAST;
-		p1.add(new JButton("Quitter"), gbc1); //add action listnener
+		p1.add(b4, gbc1);
 
 		add(p1, BorderLayout.SOUTH);
 
-		int choix = 0; // Modifier le choix dans le gestionnaire d'evenements
-
-		// choixTraitement(choix);
-
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-	}
-
-	private void choixTraitement(int choix) {
-
-		if (choix == 0) {
-			new AffichageGestionArtiste();
-		} else {
-			new AffichageGestionAlbum();
-		}
 
 	}
 }
