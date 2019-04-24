3d5dd2f9fe7f537d4aa3b2cd3952faa13df266e1
Jsbeaulne <jeansebastien321@gmail.com>
2019-04-24 00:00:39 -0400

    WTF

--- src/main/mainTP2.java
+++ src/main/mainTP2.java
@@ -6,7 +6,6 @@ public class mainTP2 {
 
 	public static void main(String[] args) {
 		new IdentificationUtilisateur();
-
 	}
 
 }
--- src/vues/IdentificationUtilisateur.java
+++ src/vues/IdentificationUtilisateur.java
@@ -1,7 +1,16 @@
 package vues;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class IdentificationUtilisateur extends JFrame{
 	private static final long serialVersionUID = 1L;
@@ -11,9 +20,63 @@ public class IdentificationUtilisateur extends JFrame{
 		super("Authentification");
 		setSize(500, 200);
 		setVisible(true);
-		setResizable(false);
+		//setResizable(false);
 		setIconImage(new ImageIcon("icon.png").getImage());
+		setLayout(new GridBagLayout());
 		
+		JLabel c1 = new JLabel("Nom d'Utilisateur : ");
+		JTextField c2 = new JTextField();
+		JLabel c3 = new JLabel("Mot de Passe : ");
+		JTextField c4 = new JTextField();
+		JButton b1 = new JButton("Se connecter");
+		b1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Poupou");
+			}
+			
+		});
+		JButton b2 = new JButton("Quitter");
+		b2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+			
+		});
+		
+		GridBagConstraints con = new GridBagConstraints();
+		
+		con.fill = GridBagConstraints.NONE;
+		con.weightx = 1;
+		con.weighty = 1;
+		con.gridx = 0;
+		con.gridy = 0;
+		con.insets = new Insets(10, 10, 10, 10);
+		
+		add(c1, con);
+		
+		con.gridx = 1;
+		con.fill = GridBagConstraints.HORIZONTAL;
+		add(c2, con);
+		
+		con.gridy = 1;
+		add(c4, con);
+		
+		con.gridx = 0;
+		con.fill = GridBagConstraints.NONE;
+		add(c3, con);
+		
+		con.gridy = 2;
+		con.fill = GridBagConstraints.HORIZONTAL;
+		add(b1, con);
+		
+		con.gridx = 1;
+		add(b2, con);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
