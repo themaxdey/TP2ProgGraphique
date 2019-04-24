12a924bdae07504a415262c1c555cadaa72a4811
themaxdey <themaxdey@users.noreply.github.com>
2019-04-23 20:04:54 -0400

    choix traitement

--- src/main/mainTP2.java
+++ src/main/mainTP2.java
@@ -1,11 +1,12 @@
 package main;
 
+import vues.ChoixTraitements;
 import vues.IdentificationUtilisateur;
 
 public class mainTP2 {
 
 	public static void main(String[] args) {
-		new IdentificationUtilisateur();
+		new ChoixTraitements();
 
 	}
 
--- src/vues/ChoixTraitements.java
+++ src/vues/ChoixTraitements.java
@@ -1,11 +1,14 @@
 package vues;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ChoixTraitements extends JFrame {
@@ -15,34 +18,65 @@ public class ChoixTraitements extends JFrame {
 	public ChoixTraitements() {
 
 		super("Choix des traitements");
-		setSize(500, 200);
+		setSize(250, 200);
 		setVisible(true);
 		setResizable(false);
 		setIconImage(new ImageIcon("icon.png").getImage());
 
-		creerChoixTraitement();
+		JPanel p = new JPanel(new GridBagLayout());
 
-		String choix = null; // Modifier le choix dans le gestionnaire d'evenements
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.fill = GridBagConstraints.VERTICAL;
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+		gbc.insets = new Insets(2, 2, 2, 2);
 
-		choixTraitement(choix);
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		p.add(new JLabel("Sélectionner la catégorie à modifier"), gbc);
+
+		gbc.gridy = 2;
+		p.add(new JButton("Artiste"), gbc);
+
+		gbc.gridy = 3;
+		p.add(new JButton("Album"), gbc);
+
+		
+
+		add(p, BorderLayout.NORTH);
+		
+		gbc.anchor = GridBagConstraints.WEST;
+
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+		gbc.insets = new Insets(2, 2, 2, 2);
+		
+		JPanel p1 = new JPanel(new GridBagLayout());
+		gbc.gridx = 1;
+		gbc.gridy = 6;
+		p1.add(new JButton("Aide en ligne"), gbc);
+		
+		gbc.gridx = 3;
+		p1.add(new JButton("Quitter"), gbc);
+		
+		add(p1, BorderLayout.SOUTH);
+
+		//pack();
+
+		int choix = 0; // Modifier le choix dans le gestionnaire d'evenements
+
+		// choixTraitement(choix);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
 
-	private void creerChoixTraitement() {
-		JPanel pNord = new JPanel(new FlowLayout());
-		JButton[] boutons = new JButton[2];
-		boutons[0] = new JButton("Un bouton");
-		boutons[1] = new JButton("Un bouton");
-		pNord.add(boutons[0]);
-		pNord.add(boutons[1]);
+	private void choixTraitement(int choix) {
 
-		add(pNord, BorderLayout.NORTH);
-
-	}
-
-	private void choixTraitement(String choix) {
-
-		if (choix.equals("gestionArtiste")) {
+		if (choix == 0) {
 			new AffichageGestionArtiste();
 		} else {
 			new AffichageGestionAlbum();
