777691dce29cb2bf8e5e28ae3385ba38998d76b6
themaxdey <themaxdey@users.noreply.github.com>
2019-04-23 22:27:50 -0400

    completion des constructeurs artistes et album

--- src/modeles/Album.java
+++ src/modeles/Album.java
@@ -1,5 +1,33 @@
 package modeles;
 
 public class Album {
-
+	String titre;
+	int annee;
+	String photo;
+	int numeroArtisteFK;
+	int numeroAlbum;
+	
+	public Album (String titre, int annee, String photo, int numeroArtisteFK, int numeroAlbum) {
+		this.titre = titre;
+		this.photo = photo;
+		this.numeroArtisteFK = numeroArtisteFK;
+		this.numeroAlbum = numeroAlbum;
+	}
+	
+	public String getTitre() {
+		return this.titre;
+	}
+	
+	public int getNumeroAlbum() {
+		return this.numeroAlbum;
+	}
+	
+	public int getNumeroArtisteFK() {
+		return numeroArtisteFK;
+	}
+	
+	public int getAnnee() {
+		return this.annee;
+	}
+	
 }
--- src/modeles/Artiste.java
+++ src/modeles/Artiste.java
@@ -1,5 +1,27 @@
 package modeles;
 
 public class Artiste {
-
+	String nom;
+	String photo;
+	int numeroArtiste;
+	String membre;
+	
+	public Artiste(String nom, String photo, int numero, String membre) {
+		this.nom = nom;
+		this.photo = photo;
+		this.numeroArtiste = numero;
+		this.membre = membre;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public String getMembre() {
+		return this.membre;
+	}
+	
+	public int getNumero() {
+		return numeroArtiste;
+	}
 }
